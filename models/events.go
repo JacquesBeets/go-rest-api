46d21c1b0c4c985a5c3d1ee9642f3abab8d343fc
Jacques Beets <jacques.beets@gmail.com>
2024-02-05 21:47:05 +0200

    Add GET and POST endpoints for events

--- models/events.go
+++ models/events.go
@@ -4,15 +4,19 @@ import "time"
 
 type Event struct {
 	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
-	DateTime    time.Time `json:"date_time"`
-	UserID      int       `json:"user_id"`
+	Title       string    `json:"title" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"date_time" binding:"required"`
+	UserID      int       `json:"userId"`
 }
 
 var events = []Event{}
 
-func (e Event) Save(){
-    events = append(events, e)
+func (e Event) Save() {
+	events = append(events, e)
+}
+
+func GetEvents() []Event {
+	return events
 }
