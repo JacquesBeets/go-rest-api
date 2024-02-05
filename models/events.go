957f6a8c9283d16cab88b90d13e1870b2d05084d
Jacques Beets <jacques.beets@gmail.com>
2024-02-05 22:36:33 +0200

    Refactor Event.Save() method to use a prepared statement for inserting data into the events table

--- models/events.go
+++ models/events.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/jacquesbeets/go-rest-api/db"
+)
 
 type Event struct {
-	ID          int       `json:"id"`
+	ID          int64       `json:"id"`
 	Title       string    `json:"title" binding:"required"`
 	Description string    `json:"description" binding:"required"`
 	Location    string    `json:"location" binding:"required"`
@@ -13,8 +17,22 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events (title, description, location, date_time, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(e.Title, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := res.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetEvents() []Event {
